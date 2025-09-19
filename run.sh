81c04bea2c235e7356497ffd6936766d74d5baf8
minhchuxuan <64071806+minhchuxuan@users.noreply.github.com>
2025-09-19 14:04:59 +0700

    first commit

---
+++ run.sh
@@ -0,0 +1,5 @@
+python main.py --model XTRA --seed 0 --dataset Amazon_Review --weight_cluster 10 --weight_beta 15 --weight_InfoNCE 85 --device 0
+python main.py --model XTRA --seed 0 --dataset ECNews --weight_cluster 5 --weight_beta 7 --weight_InfoNCE 80 --device 0
+python main.py --model XTRA --seed 7 --dataset Rakuten_Amazon --weight_cluster 10 --weight_beta 7 --weight_InfoNCE 70 --device 0
+
+
